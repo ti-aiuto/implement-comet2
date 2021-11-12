06315fda9ba2ab813f581abfff584bef454c7596
ti-aiuto <ti.aiuto@outlook.jp>
2021-11-12 23:10:00 +0900

    :pencil2: :タイマー変更

--- pj3/pj3.vhd
+++ pj3/pj3.vhd
@@ -6,13 +6,15 @@ entity pj3 is
 	(
 		CLK_IN : in std_logic;
 		SEG7A : out std_logic_vector(6 downto 0);
-		DIGITA_SELECT : out std_logic_vector(5 downto 0)
+		DIGITA_SELECT : out std_logic_vector(5 downto 0);
+		SEG7B : out std_logic_vector(6 downto 0);
+		DIGITB_SELECT : out std_logic_vector(5 downto 0)
 	);
 end pj3;
 
 architecture RTL of pj3 is
 
-component clock_down
+component clock_down_dynamyc_7seg
 	port(
 		CLK_IN : in std_logic;
 		CLK_OUT : out std_logic
@@ -30,11 +32,16 @@ end component;
 signal CLK_SLOW : std_logic;
 
 begin
-	CLOCK_COMPONENT : clock_down port map(CLK_IN => CLK_IN, CLK_OUT => CLK_SLOW);
+	CLOCK_COMPONENT : clock_down_dynamyc_7seg port map(CLK_IN => CLK_IN, CLK_OUT => CLK_SLOW);
 	
 	DEC1 : bin_16_dec_dynamic_6 port map( CLK_IN => CLK_SLOW,
 	BIN_IN => "0010011010010100", 
 	SEG7 => SEG7A, 
 	DIGIT_SELECT => DIGITA_SELECT);
+
+		DEC2: bin_16_dec_dynamic_6 port map( CLK_IN => CLK_SLOW,
+	BIN_IN => "1101010000110001", 
+	SEG7 => SEG7B, 
+	DIGIT_SELECT => DIGITB_SELECT);
 end RTL;
 	
