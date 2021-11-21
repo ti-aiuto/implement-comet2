0f519abe32593a67b0b64ade86cb70bad1dd379f
ti-aiuto <ti.aiuto@outlook.jp>
2021-11-21 21:39:34 +0900

    :pencil2: OFのセット実装

--- pj3/alu.vhd
+++ pj3/alu.vhd
@@ -39,28 +39,58 @@ component adder_16bit is
 	);
 end component;
 
-signal SET_ALU_ADDER_CI_FLAG : std_logic;
+component multiplexer_1bit_2ways is
+	port(
+		SELECTOR : in std_logic;
+		DATA_IN_1 : in std_logic;
+		DATA_IN_2 : in std_logic;
+		DATA_OUT : out std_logic
+	);
+end component;
+
 signal DATA_B_NEGATED : std_logic_vector(15 downto 0);
-signal USE_NEGATED_DATAB_FLAG: std_logic;
 signal DATA_B_OR_NEGATED_DATA_B : std_logic_vector(15 downto 0);
 
-begin
-	-- 引き算
-	USE_NEGATED_DATAB_FLAG <= SUB_FLAG;
-	SET_ALU_ADDER_CI_FLAG <= SUB_FLAG;
-	
-	NOT_NEGATE_DATAB : not_16bit port map(DATA_IN => DATA_IN_B, DATA_OUT => DATA_B_NEGATED);
-	
-	MX_NEGATE_DATAB : multiplexer_16bit_2ways port map( SELECTOR => USE_NEGATED_DATAB_FLAG, 
-	DATA_IN_1 => DATA_IN_B, 
-	DATA_IN_2 => DATA_B_NEGATED, 
-	DATA_OUT => DATA_B_OR_NEGATED_DATA_B);
-	
-	ALU_ADDER : adder_16bit port map( CI => SET_ALU_ADDER_CI_FLAG, 
-	AIN => DATA_IN_A, 
-	BIN => DATA_B_OR_NEGATED_DATA_B, 
-	SUM(15 downto 0) => DATA_OUT); -- TODO: ここでOFとかのセット必要
+signal INTERNAL_ADD_SUB_DATA : std_logic_vector(15 downto 0);
+signal INTERNAL_LOGICAL_OF: std_logic;
+signal INTERNAL_ARITHMETIC_OF: std_logic;
 
-	OF_OUT <= '0'; -- TODO: set
+signal DATA_A_FLAG : std_logic;
+signal DATA_B_FLAG : std_logic;
+
+begin
+	NOT_NEGATE_DATAB : not_16bit port map(
+		DATA_IN => DATA_IN_B, 
+		DATA_OUT => DATA_B_NEGATED
+	);
+	
+	MX_NEGATE_DATAB : multiplexer_16bit_2ways port map( 
+		SELECTOR => SUB_FLAG, 
+		DATA_IN_1 => DATA_IN_B, 
+		DATA_IN_2 => DATA_B_NEGATED, 
+		DATA_OUT => DATA_B_OR_NEGATED_DATA_B
+	);
+	
+	ALU_ADDER : adder_16bit port map( 
+		CI => SUB_FLAG, 
+		AIN => DATA_IN_A, 
+		BIN => DATA_B_OR_NEGATED_DATA_B, 
+		SUM(15 downto 0) => INTERNAL_ADD_SUB_DATA, 
+		SUM(16) => INTERNAL_LOGICAL_OF
+	);
+
+	DATA_A_FLAG <= DATA_IN_A(15);
+	DATA_B_FLAG <= DATA_IN_B(15) xor SUB_FLAG; -- 負数かつ加算、正数かつ減算のとき1
+	INTERNAL_ARITHMETIC_OF <= NOT(DATA_A_FLAG XOR DATA_B_FLAG) -- 正負が同じかつ
+		AND (DATA_A_FLAG XOR INTERNAL_ADD_SUB_DATA(15)); -- 正負が変わっているときあふれ
+
+	OF_SELECTOR : multiplexer_1bit_2ways port map(
+		SELECTOR => LOGICAL_CALC_FLAG, 
+		DATA_IN_1 => INTERNAL_ARITHMETIC_OF, 
+		DATA_IN_2 => INTERNAL_LOGICAL_OF, 
+		DATA_OUT => OF_OUT
+	);
+
+	DATA_OUT <= INTERNAL_ADD_SUB_DATA;
 end RTL;
 
