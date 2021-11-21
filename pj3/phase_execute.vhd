7ad77f529d8651a2c3e0d63afa0fd7b3d033b675
ti-aiuto <ti.aiuto@outlook.jp>
2021-11-21 20:15:20 +0900

    :pencil2: リファクタ

--- pj3/phase_execute.vhd
+++ pj3/phase_execute.vhd
@@ -3,7 +3,8 @@ use IEEE.std_logic_1164.all;
 
 entity phase_execute is
 	port(
-		CLK : in std_logic;
+		CLK : in std_logic; 
+		RESET_IN : in std_logic;
 		EFFECTIVE_ADDR_IN : in std_logic_vector(15 downto 0);
 		RAM_IN : in std_logic_vector(15 downto 0);
 		GRA_IN : in std_logic_vector(15 downto 0);
@@ -17,8 +18,7 @@ entity phase_execute is
 		NEXT_PR : out std_logic_vector(15 downto 0);
 		WRITE_GR_FLAG : out std_logic;
 		WRITE_PR_FLAG : out std_logic; 
-		WRITE_FR_FLAG : out std_logic; 
-		RESET_IN : in std_logic
+		WRITE_FR_FLAG : out std_logic
 	);
 end phase_execute;
 
@@ -103,8 +103,6 @@ component adder_16bit is
 	);
 end component;
 
-
-signal USE_RAM_ADDR_AS_DATA_FLAG : std_logic;
 signal EFFECTIVE_ADDR_OR_RAM_OUT : std_logic_vector(15 downto 0);
 
 signal USE_ZERO_AS_GRA_FLAG: std_logic;
@@ -119,7 +117,6 @@ signal INTERNAL_ALU_DATA_OR : std_logic;
 signal INTERNAL_NEXT_OF: std_logic;
 signal INTERNAL_NEXT_SF: std_logic;
 signal INTERNAL_NEXT_ZF: std_logic;
-signal INTERNAL_FR_DATA : std_logic_vector(2 downto 0);
 
 signal MAIN_OP_IS_JP_FLAG : std_logic;
 signal OP_IS_JMI_FLAG : std_logic;
@@ -144,35 +141,43 @@ signal WORDS_COUNT_TO_ADD : std_logic_vector(15 downto 0);
 signal PR_WORD_ADDED : std_logic_vector(15 downto 0);
 signal NEXT_PR_OR_JP_ADDR : std_logic_vector(15 downto 0);
 
-signal WRITE_PR_OP_OR_RESET_FLAG : std_logic;
 
 begin
-
-	USE_RAM_ADDR_AS_DATA_FLAG <= OP_IS_LAD_FLAG;
 	USE_ZERO_AS_GRA_FLAG <= OP_IS_LAD_FLAG OR OP_IS_LD_FLAG;
-	USE_RAM_AS_GRB_FLAG <= OP_IS_LAD_FLAG;
+	USE_RAM_AS_GRB_FLAG <= OP_IS_LAD_FLAG; -- TODO: データBをRAMから読み込む2語命令をここに追加
 	
-	RAM_MX : multiplexer_16bit_2ways port map( SELECTOR => USE_RAM_ADDR_AS_DATA_FLAG, 
-	DATA_IN_1 => RAM_IN,
-	DATA_IN_2 => EFFECTIVE_ADDR_IN,
-	DATA_OUT	=> EFFECTIVE_ADDR_OR_RAM_OUT);
+	WRITE_GR_FLAG <= OP_NEEDS_WRITE_GR_FLAG;
+	WRITE_PR_FLAG <= RESET_IN OR OP_NEEDS_WRITE_PR_FLAG;
+	WRITE_FR_FLAG <= OP_NEEDS_WRITE_FR_FLAG;	
+	
+	RAM_MX : multiplexer_16bit_2ways port map(
+		SELECTOR => OP_IS_LAD_FLAG, -- LADの場合はRAM値ではなくRAM番地をデータBとして使う
+		DATA_IN_1 => RAM_IN,
+		DATA_IN_2 => EFFECTIVE_ADDR_IN,
+		DATA_OUT	=> EFFECTIVE_ADDR_OR_RAM_OUT
+	);
 
-	MX_GRA_OR_ZERO : multiplexer_16bit_2ways port map( SELECTOR => USE_ZERO_AS_GRA_FLAG, 
-	DATA_IN_1 => GRA_IN, 
-	DATA_IN_2 => "0000000000000000", 
-	DATA_OUT => GRA_OR_ZERO);
+	MX_GRA_OR_ZERO : multiplexer_16bit_2ways port map(
+		SELECTOR => USE_ZERO_AS_GRA_FLAG, 
+		DATA_IN_1 => GRA_IN, 
+		DATA_IN_2 => "0000000000000000", 
+		DATA_OUT => GRA_OR_ZERO
+	);
 	
-	USE_RAM_AS_GRB_FLAG <= OP_IS_LAD_FLAG;
-	MX_GRB_OR_RAM : multiplexer_16bit_2ways port map( SELECTOR => USE_RAM_AS_GRB_FLAG, 
-	DATA_IN_1 => GRB_IN, 
-	DATA_IN_2 => EFFECTIVE_ADDR_OR_RAM_OUT, 
-	DATA_OUT => GRB_OR_RAM);
+	MX_GRB_OR_RAM : multiplexer_16bit_2ways port map(
+		SELECTOR => USE_RAM_AS_GRB_FLAG, 
+		DATA_IN_1 => GRB_IN, 
+		DATA_IN_2 => EFFECTIVE_ADDR_OR_RAM_OUT, 
+		DATA_OUT => GRB_OR_RAM
+	);
 				
-	ALU_INSTANCE : alu port map(SUB_FLAG => OP_IS_SUB_FLAG or MAIN_OP_IS_CP_FLAG, -- 引き算に切り替え
-	DATA_IN_A => GRA_OR_ZERO, 
-	DATA_IN_B => GRB_OR_RAM, 
-	DATA_OUT => INTERNAL_ALU_DATA, 
-	OF_OUT => INTERNAL_ALU_OF);
+	ALU_INSTANCE : alu port map(
+		SUB_FLAG => OP_IS_SUB_FLAG or MAIN_OP_IS_CP_FLAG, -- 引き算に切り替え
+		DATA_IN_A => GRA_OR_ZERO, 
+		DATA_IN_B => GRB_OR_RAM, 
+		DATA_OUT => INTERNAL_ALU_DATA, 
+		OF_OUT => INTERNAL_ALU_OF
+	);
 
 	OR_FR_IN : or_in_16bit_out_1bit port map( 
 		DATA_IN => INTERNAL_ALU_DATA, 
@@ -183,12 +188,20 @@ begin
 	INTERNAL_NEXT_OF <= INTERNAL_ALU_OF;
 	INTERNAL_NEXT_SF <= INTERNAL_ALU_DATA(15); -- 最上位ビット
 	INTERNAL_NEXT_ZF <= not INTERNAL_ALU_DATA_OR;
-	INTERNAL_FR_DATA(0) <= INTERNAL_NEXT_OF;
-	INTERNAL_FR_DATA(1) <= INTERNAL_NEXT_SF;
-	INTERNAL_FR_DATA(2) <= INTERNAL_NEXT_ZF;
 	
-	ALU_DATA_REGISTER : register_16 port map(CLK_IN => CLK, WRITE_FLAG => '1', DATA_IN => INTERNAL_ALU_DATA, DATA_OUT => NEXT_DATA);
-	ALU_FR_REGISTER : register_4 port map(CLK_IN => CLK, WRITE_FLAG => '1', DATA_IN => "0" & INTERNAL_FR_DATA, DATA_OUT(2 downto 0) => NEXT_FR);
+	ALU_DATA_REGISTER : register_16 port map(
+		CLK_IN => CLK, 
+		WRITE_FLAG => '1', 
+		DATA_IN => INTERNAL_ALU_DATA, 
+		DATA_OUT => NEXT_DATA
+	);
+	
+	ALU_FR_REGISTER : register_4 port map(
+		CLK_IN => CLK, 
+		WRITE_FLAG => '1', 
+		DATA_IN => "0" & INTERNAL_NEXT_ZF & INTERNAL_NEXT_SF & INTERNAL_NEXT_OF, 
+		DATA_OUT(2 downto 0) => NEXT_FR
+	);
 	
 	PARSE_OP : parse_op_as_flag port map(
 		MAIN_OP => MAIN_OP_IN,
@@ -213,15 +226,17 @@ begin
 
 	WORD_COUNT_MUX : multiplexer_16bit_2ways port map(
 		SELECTOR => OP_LENGTH_IS_TWO_FLAG, 
-		DATA_IN_1 => "0000000000000001", 
-		DATA_IN_2 => "0000000000000010", 
+		DATA_IN_1 => "0000000000000001", -- 1語命令
+		DATA_IN_2 => "0000000000000010", -- 2語命令
 		DATA_OUT => WORDS_COUNT_TO_ADD
 	);
-	PR_ADDER : adder_16bit port map( CI => '0', AIN => PR_IN, BIN => WORDS_COUNT_TO_ADD, SUM(15 downto 0) => PR_WORD_ADDED);	
-	
-	-- このへんはWBの立ち上がりで使うものなのでレジスタで覚えない
-	WRITE_PR_OP_OR_RESET_FLAG <= RESET_IN OR OP_NEEDS_WRITE_PR_FLAG;
-	
+	PR_ADDER : adder_16bit port map(
+		CI => '0', 
+		AIN => PR_IN, 
+		BIN => WORDS_COUNT_TO_ADD, 
+		SUM(15 downto 0) => PR_WORD_ADDED
+	);	
+		
 	USE_JP_FLAG <= (OP_IS_JMI_FLAG and FR_IN(1))
 		or (OP_IS_JNZ_FLAG and not FR_IN(2))
 		or (OP_IS_JZE_FLAG and FR_IN(2))
@@ -237,14 +252,10 @@ begin
 	);
 	
 	-- execにうつしてもいいかも
-	NEXT_PR_MX : multiplexer_16bit_2ways port map( SELECTOR => RESET_IN, 
-	DATA_IN_1 => NEXT_PR_OR_JP_ADDR, 
-	DATA_IN_2 => "0000000000000000", -- reset
-	DATA_OUT => NEXT_PR );
-	
-	WRITE_GR_FLAG <= OP_NEEDS_WRITE_GR_FLAG;
-	WRITE_PR_FLAG <= WRITE_PR_OP_OR_RESET_FLAG;
-	WRITE_FR_FLAG <= OP_NEEDS_WRITE_FR_FLAG;	
-	
-	
+	NEXT_PR_MX : multiplexer_16bit_2ways port map(
+		SELECTOR => RESET_IN, 
+		DATA_IN_1 => NEXT_PR_OR_JP_ADDR, 
+		DATA_IN_2 => "0000000000000000", -- reset
+		DATA_OUT => NEXT_PR
+	);
 end RTL;
